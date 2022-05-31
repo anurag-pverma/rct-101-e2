e3a7bd0af893e6e014cfae5ec1bf43f03b13790b
Anurag Verma <anurag.premaverma@gmail.com>
2022-05-31 21:02:45 +0530

    .

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,69 +1,60 @@
-import React from "react";
-import axios from "axios";
-import { useState, useEffect } from "react";
+import {
+  Text,
+  Image,
+  Box,
+  Stack,
+  Heading,
+  Tag,
+  TagLabel,
+  Flex,
+} from "@chakra-ui/react";
 
-const Product = () => {
+const Product = ({ imageSrc, title, category, price, gender }) => (
+  <Stack boxShadow={"2xl"} rounded={"lg"} p={6} data-cy="product">
+    <Image
+      rounded={"lg"}
+      height={230}
+      objectFit={"cover"}
+      src={imageSrc}
+      data-cy="product-image"
+    />
 
-  const [listOfProduct, setListOFProduct] = useState([]);
-  const url = `http://localhost:8080/products`;
+    <Flex justifyContent={"space-between"}>
+      <Text
+        color="teal.600"
+        textTransform="uppercase"
+        data-cy="product-category"
+      >
+        {category}
+      </Text>
+      {gender && (
+        <Tag
+          size="sm"
+          variant="subtle"
+          colorScheme="cyan"
+          data-cy="product-gender"
+        >
+          <TagLabel textTransform="uppercase">{gender}</TagLabel>
+        </Tag>
+      )}
+    </Flex>
 
-
-  
-  useEffect(()=>{
-    axios.get(url)
-    .then(res=>{
-      // console.log(res);
-      setListOFProduct(res.data);
-    })
-  },[])
-  // TODO: Remove below const and instead import them from chakra
-
-
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
-  return (
-    <>
-      {
-        listOfProduct &&  listOfProduct.map ((product,index)=>{
-          return <div key={index} style={{ display:"grid"}}>
-            <div> 
-              
-               <img src={product.imageSrc} alt="" /> 
-
-               <p>{product.category}</p>
-               <p>{product.price}</p>
-               <p>{product.title}</p>
-               <p>{product.gender}</p>
-
-            </div>
-
-          </div>
-        })
-        // console.log(listOfProduct)
-
-        // category: "pant"
-// gender: "male"
-// id: 1
-// imageSrc: "https://picsum.photos/seed/picsum6/420/260"
-// price: "23.00"
-// title: "react tshirt"
-      }
-    <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
-      <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
-      </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
-    </Stack>
-    </>
-  );
-};
+    <Heading
+      color="teal.300"
+      size="md"
+      textTransform="capitalize"
+      data-cy="product-title"
+    >
+      {title}
+    </Heading>
+    <Box data-cy="product-price">
+      {"Rs. "}
+      {price}
+      <Box as="span" color="gray.600" fontSize="sm">
+        / unit
+      </Box>
+    </Box>
+  </Stack>
+);
 
 export default Product;
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,30 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Flex, Grid } from "@chakra-ui/react";
+import axios from "axios";
 
+import Product from "./Product";
 import AddProduct from "./AddProduct";
 import Pagination from "./Pagination";
-import Product from "./Product";
-
 
 const Products = () => {
-  // // TODO: Remove below const and instead import them from chakra
+  const [pageFilter, setPageFilter] = useState({
+    pageNumber: 1,
+    limit: 3,
+    totalCount: 0,
+  });
+  const [products, setProducts] = useState([]);
 
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  const { pageNumber, limit } = pageFilter;
 
+  const handleOnAdd = (body) => {
+    axios.post("http://localhost:8080/products", body).then((r) => {
+      setProducts([...products, r.data]);
+    });
+  };
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await axios.get(
+        `http://localhost:8080/products?_page=${pageNumber}&_limit=${limit}`
+      );
+      setPageFilter({
+        ...pageFilter,
+        totalCount: Number(response.headers["x-total-count"]),
+      });
+
+      setProducts(response.data);
+    };
+    getProducts();
+    
+  }, [pageNumber, limit]);
   return (
-    <>
-      {/* <h1>hellohojoljljoljaohoj</h1> */}
-        <AddProduct/>
-      <Product/>
-
-      {/* <Flex> */}
-        {/*  AddProduct */}
-        {/* <Grid>List of Products</Grid> */}
-        {/* Pagination */}
-        {/* <Pagination/> */}
-      {/* </Flex> */}
-    </>
+    <Flex
+      direction="column"
+      justifyContent="center"
+      alignItems="center"
+      borderColor="gray.200"
+      borderWidth={1}
+      borderStyle="solid"
+      borderRadius={16}
+      p={4}
+      mt={4}
+    >
+      <AddProduct add={handleOnAdd} />
+      <Grid w="full" gridGap={4} gridTemplateColumns="1fr 1fr 1fr">
+        {products.map((p) => (
+          <Product key={p.id} {...p} />
+        ))}
+      </Grid>
+      <Pagination pageFilter={pageFilter} setPageFilter={setPageFilter} />
+    </Flex>
   );
 };
 
 export default Products;
+
